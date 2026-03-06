62a9944ea213ff089048c731590fc99dafff65ce
FacundesPedro <pedro.f.facundes1@gmail.com>
2026-03-05 22:24:31 -0300

    [feat/refactor]: docker-compose, .env, challenge repository

--- src/service/adapters/routes/challenge.routes.ts
+++ src/service/adapters/routes/challenge.routes.ts
@@ -5,7 +5,7 @@ const router = Router();
 /** GET */
 
 router.get("/", (req, res) => {challengeController.getAll(req, res)});
-router.get("/:id", (req, res) => {challengeController.getById(req, res)});
+router.get("/get-by-id/:id", (req, res) => {challengeController.getById(req, res)});
 // router.put("/", (req, res) => challengeController.getById(req, res));
 // router.delete("/", (req, res) => challengeController.getById(req, res));)
 
