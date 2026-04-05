dfbf9e9530c96974a2257d690c918d664ae85d86
Rodrigo de Jesus Macedo <roromacedo1207@gmail.com>
2026-04-05 03:10:26 -0300

    fix: corrige testes E2E e padroniza ConsultaRepository
    - Adiciona mocks do Firebase nos 5 testes e2e que importavam app
      sem mockar firebase/app, firebase/auth e firebase/firestore
    - ConsultaPostgresRepository.getById: retorna null -> lança erro,
      consistente com todos os outros repositórios
    - IConsultaPort: atualiza assinatura getById para Promise<Consulta>

--- src/service/core/ports/consulta.port.ts
+++ src/service/core/ports/consulta.port.ts
@@ -2,6 +2,6 @@ import { Consulta } from "../domain/consulta.entity";
 
 export interface IConsultaPort {
   getAll(): Promise<Consulta[]>;
-  getById(id: number): Promise<Consulta | null>;
+  getById(id: number): Promise<Consulta>;
   getByCapituloId(idCapitulo: number): Promise<Consulta[]>;
 }
